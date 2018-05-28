54dd8f11c0e5723a3f18a99d4addffed72c938ee
Sheikh Ibrahim <isheikh03@gmail.com>
2018-05-28 07:42:24 +0600

    register, login logout status update

--- assets/js/firebase_connection.js
+++ assets/js/firebase_connection.js
@@ -10,89 +10,11 @@ var config = {
 
 firebase.initializeApp(config);
 
-  //create firebase references
-  var Auth = firebase.auth(); 
-  var dbRef = firebase.database();
-  /*var contactsRef = dbRef.ref('contacts')*/
-  var usersRef = dbRef.ref('users');
-  var auth = null;
+/*------------create firebase references-------------*/
+var auth        = firebase.auth(); 
+var dbRef       = firebase.database();
+var usersRef    = dbRef.ref('users');
+var messageRef  = dbRef.ref('messages');
+var messageRef  = dbRef.ref('friends');
 
-/*--------firebase register function-------------*/
-  $('#user_register').on('click', function () {
-      //alert("start");
-    //e.preventDefault();
-      
-    var data = {
-      name: $('#name').val(), //get the email from Form
-      email: $('#email').val(), //get the email from Form
-    };
-    var passwords = {
-      password : $('#password').val(), //get the pass from Form
-      cPassword : $('#re_password').val(), //get the confirmPass from Form
-    }
-    
-    console.log(passwords.password + "  " +passwords.cPassword);
-    
-    if( data.email != '' && passwords.password != ''  && passwords.cPassword != '' ){
-      if( passwords.password == passwords.cPassword ){
-        //create the user
-        
-          console.log("if if -------true");
-          
-        firebase.auth()
-          .createUserWithEmailAndPassword(data.email, passwords.password)
-          .then(function(user){
-            //now user is needed to be logged in to save data
-            console.log("Authenticated successfully with payload:", user);
-            auth = user;
-            
-            //now saving the profile data
-            usersRef
-              .child(auth.user.uid)
-              .set(data)
-              .then(function(){
-                console.log("User Information Saved:", auth.user.uid);
-              })
-           
-            console.log("Successfully created user account with uid:", auth.user.uid);
-            
-          }).catch(function(error){
-            console.log("Error creating user:", error);
-          });
-      } else {
-          console.log("password and confirm password didn't match :", error);
-        //password and confirm password didn't match
-      }
-    }  
-  });  
 
-/*--------firebase login function-------------*/
-$('#do_login').on('click', function () {
-      
-    if( $('#email').val() != '' && $('#password').val() != '' ){
-      //login the user
-      var data = {
-        email: $('#email').val(),
-        password: $('#password').val()
-      };
-      console.log(data.email+" --------------"+data.password);  
-          
-      Auth.signInWithEmailAndPassword(data.email, data.password)
-          .then(function(authData) {
-         
-         console.log("Authenticated successfully with payload:", authData.user.uid);
-          
-          redirectData(authData);
-          
-        }).catch(function(error) {
-          console.log("Login Failed!", error);
-          
-        });
-    }
-  });
-    
-/*--------page redirect function-------------*/    
-function redirectData(authData){
-//passing the variable into the window.location URL
-    window.location.replace("file:///C:/Users/USER/Documents/GitHub/gossiping_web/index.html");
-}
