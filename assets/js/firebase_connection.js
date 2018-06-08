ab480977707e8296c0880a9f2c1f4943ebacb83e
Hridoy <hridoy.jurain@yahoo.com>
2018-06-09 03:10:34 +0600

    formated app.js

--- assets/js/firebase_connection.js
+++ assets/js/firebase_connection.js
@@ -4,20 +4,25 @@ firebase.initializeApp({
     apiKey: "AIzaSyBgM3KN6dBZi13eYK4cJA8MRoSX5kvsK4o",
     authDomain: "gossiping-b018c.firebaseapp.com",
     projectId: "gossiping-b018c",
-     storageBucket: "gossiping-b018c.appspot.com"
+    storageBucket: "gossiping-b018c.appspot.com"
 });
 
 
 
 /*------------create firebase references-------------*/
-var auth        = firebase.auth(); 
+const auth        = firebase.auth(); 
 
-var storageRef = firebase.storage().ref();
+const storageRef = firebase.storage().ref();
 
-var dbRef       = firebase.firestore();
-var usersRef    = dbRef.collection('users');
-var messagesRef = dbRef.collection('messages');
-var friendsRef  = dbRef.collection('friends');
+const dbRef       = firebase.firestore();
+const settings = {timestampsInSnapshots: true}; 
+
+      dbRef.settings(settings);
+
+const usersRef    = dbRef.collection('users');
+const messagesRef = dbRef.collection('messages');
+const friendsRef  = dbRef.collection('friends');
+
 
 
 
--- assets/js/user_login.js
+++ assets/js/user_login.js
@@ -1,39 +1,34 @@
 /*--------firebase login function-------------*/
-$('#login_password').keypress(function(e){
-   if(e.keyCode == 13) {
-       doLogin();
-   }
+$('#login_password').keypress(function (e) {
+    if (e.keyCode == 13) {
+        doLogin();
+    }
 });
 $('#login_btn').on('click', function () {
-   doLogin();
+    doLogin();
 });
 
 
-function doLogin(){
-    
+function doLogin() {
+
     var loginInfo = {
         email: $('#login_email').val(),
         password: $('#login_password').val()
     };
-    
-    if( loginInfo.email !== '' && loginInfo.password !== '' ){
+
+    if (loginInfo.email !== '' && loginInfo.password !== '') {
 
         auth.signInWithEmailAndPassword(loginInfo.email, loginInfo.password)
-          .then(function(authData) {
-            
-            usersRef.doc(authData.user.uid).update({
-                "is_active": "Online"
-            }).then(function() {
-                console.log("Document successfully updated!");
-            });
+            .then(function (authData) {
 
-            loadPage("pages/app.html");
-            
-            console.log("Login success!");
-            
-        }).catch(function(error) {
-            
-            console.log("Login Failed!", error);
-        });
+                usersRef.doc(authData.user.uid).update({
+                    "is_active": "Online"
+                })
+
+                loadPage("pages/app.html");
+
+            }).catch(function (error) {
+                console.log("Login Failed!", error);
+            });
     }
 }
